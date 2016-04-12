88358a36d9bcc1c8565983ae23ad9f06f6647800
Deepak Suwal <Deepak Suwal@localhost>
2016-04-12 13:00:59 -0500

    ransack added

--- db/schema.rb
+++ db/schema.rb
@@ -131,8 +131,6 @@ ActiveRecord::Schema.define(version: 20160411213615) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
