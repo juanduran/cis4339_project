5d8dd568bb01bd8daac3933e2a0b80da2ba15ba3
Deepak Suwal <Deepak Suwal@localhost>
2016-04-13 14:59:52 -0500

    cocoon gem

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,4 +2,6 @@ class Customer < ActiveRecord::Base
   belongs_to :business
   has_many :estimates
   has_many :invoices
+  accepts_nested_attributes_for :estimates, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :invoices, allow_destroy: true, reject_if: :all_blank
 end
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,5 +1,5 @@
 class Vehicle < ActiveRecord::Base
 
   has_many :vehicle_checkouts
-
+  accepts_nested_attributes_for :vechile_checkouts, allow_destroy: true, reject_if: :all_blank
 end
