a8602f17dd7a1cc1b91dcae6dc7caf84e3f183df
Deepak Suwal <Deepak Suwal@localhost>
2016-04-13 19:06:52 -0500

    updating

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,6 +4,7 @@ class Job < ActiveRecord::Base
   belongs_to :project
   has_many :invoices
   has_many :line_items
-  accepts_nested_attributes_for :invoices, allow_destroy: true, reject_if: :all_blank
+
   accepts_nested_attributes_for :line_items, allow_destroy: true, reject_if: :all_blank
+
 end
