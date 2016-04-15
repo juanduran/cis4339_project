8f46c6e1fb699cb7c39a38bca534ee3a0312d270
Juan Duran <juanfduran@live.com>
2016-04-15 15:27:27 -0500

    Migrations changed, removed jobs table, invoices now serves function of job table

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,7 +1,7 @@
 class Crew < ActiveRecord::Base
 
   has_many :employees
-  has_many :jobs
+  has_many :invoices
   has_many :vehicle_checkouts
 accepts_nested_attributes_for :vehicle_checkouts, allow_destroy: true, reject_if: :all_blank
 accepts_nested_attributes_for :jobs, allow_destroy: true, reject_if: :all_blank
