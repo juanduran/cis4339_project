20b0eefbb3b6619710b51de4f211d5a361c5a271
Juan Duran <juanfduran@live.com>
2016-04-15 15:53:00 -0500

    Fixed errors involving removing jobs

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -4,6 +4,5 @@ class Crew < ActiveRecord::Base
   has_many :invoices
   has_many :vehicle_checkouts
 accepts_nested_attributes_for :vehicle_checkouts, allow_destroy: true, reject_if: :all_blank
-accepts_nested_attributes_for :jobs, allow_destroy: true, reject_if: :all_blank
 accepts_nested_attributes_for :employees, allow_destroy: true, reject_if: :all_blank
 end
