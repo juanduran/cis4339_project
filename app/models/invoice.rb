a2f77278b8e4d3205d4d3b5dd23aac33af71718c
Juan Duran <juanfduran@live.com>
2016-04-17 13:43:53 -0500

    Working on invoice

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,7 +2,10 @@ class Invoice < ActiveRecord::Base
 
   belongs_to :project
   belongs_to :crew
-  has_many :line_items
+  has_many :line_items, :dependent => :destroy
   belongs_to :customer
-validates :date, :invoice_total, :terms, presence: true
+
+  accepts_nested_attributes_for :line_items, allow_destroy: true
+
+validates :date, :customer_id, :crew_id, presence: true
 end
