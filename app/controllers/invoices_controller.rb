c49cfb9473cfd494293e6ca28d1ec1d8898ec822
Juan Duran <juanfduran@live.com>
2016-04-17 12:05:48 -0500

    Starting to fix issues with key errors

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -74,6 +74,6 @@ class InvoicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def invoice_params
-      params.require(:invoice).permit(:customer_id, :date, :invoice_total, :terms, :project_id, :job_id, :user_id)
+      params.require(:invoice).permit(:customer_id, :date, :invoice_total, :terms, :project_id, :crew_id)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,10 @@
 r1 = Role.create({name: "Admin", description: "Can read items"})
 r2 = Role.create({name: "Employee", description: "Can read and create items. Can update and destroy own items"})
 
-e1 = (Employee.create({employee_name: "Matthew", crew_id: 1}))
+e1 = (Employee.create({employee_name: "Matthew", employee_phone: "7458445", crew_id: 1}))
+e1 = (Employee.create({employee_name: "Deepak", employee_phone: "74585844", crew_id: 1}))
+
+c1 = (Crew.create({crew_name: "Juanito"}))
 
 u1 = User.create({email: "santoyo@sts.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r1.id, employee_id: 1})
 u2 = User.create({email: "employee@sts.com", password: "aaaaaaaa", password_confirmation: "aaaaaaaa", role_id: r2.id, employee_id: 2})
