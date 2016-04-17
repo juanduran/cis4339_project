83d8ab3e08678f885a1e83f5a16acda8a3479962
Juan Duran <juanfduran@live.com>
2016-04-17 18:19:52 -0500

    fixed prawn pdf needs total

--- app/pdfs/estimate_pdf.rb
+++ app/pdfs/estimate_pdf.rb
@@ -1,17 +1,44 @@
 class EstimatePdf < Prawn::Document
 
-  def initialize(estimate)
+  def initialize(estimate, view)
     super(top_margin: 70)
     @estimate = estimate
-    estimate_pdf
+    @view = view
+    estimate_number
+    line_items
   end
 
-  def estimate_pdf
+  def estimate_number
     text "Estimate \##{@estimate.id}", size: 30, style: :bold
-    move_down 20
-
   end
 
+
+  def line_items
+    move_down 20
+    table line_item_rows do
+      row(0).font_style = :bold
+      columns(1..3).align = :right
+      self.row_colors = ["DDDDDD", "FFFFFF"]
+      self.header = true
+    end
+  end
+
+  def price(number)
+    @view.number_to_currency(number)
+  end
+
+  def line_item_rows
+    [["Service", "Qty", "Unit Price"]] +
+        @estimate.estimate_line_items.map do |item|
+          [item.service.name, item.quantity, price(item.service.price)]
+        end
+  end
+
+
+  def total_price
+    move_down(20)
+    text "Total Price #{@estimate.total}"
+  end
 
 
 end
