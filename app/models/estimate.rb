936cbcd4f856983a26e25a28e965cb3fe445469d
Juan Duran <juanfduran@live.com>
2016-04-17 16:10:24 -0500

    updated edit forms

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -2,7 +2,7 @@ class Estimate < ActiveRecord::Base
 
   belongs_to :customer
   has_many :estimate_line_items
-  validates :description, :quantity, presence: true
+  validates :description, presence: true
 
   accepts_nested_attributes_for :estimate_line_items, allow_destroy: true
 
