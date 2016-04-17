c6df91e9f13dea211dfe4bacd54fcbe58deaa743
Juan Duran <juanfduran@live.com>
2016-04-17 12:05:48 -0500

    Starting to fix issues with key errors

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,7 +2,7 @@ class Role < ActiveRecord::Base
 
   has_many :employees
   accepts_nested_attributes_for :employees, allow_destroy: true, reject_if: :all_blank
-  validates :last_name, :first_name, :position, :phone_number, :wage, presence: true
+  validates :name, :description, presence: true
   def admin?
     self.role.name == "Admin"
   end
