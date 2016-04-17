bb81d11cd1d8d4a79879b45239feab1186b1ecbe
Juan Duran <juanfduran@live.com>
2016-04-17 15:47:42 -0500

    Finished estimate form formatting

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -2,7 +2,7 @@ class Estimate < ActiveRecord::Base
 
   belongs_to :customer
   has_many :estimate_line_items
-  validates :potential_customer, :terms, :description, :quantity, :total, presence: true
+  validates :description, :quantity, presence: true
 
   accepts_nested_attributes_for :estimate_line_items, allow_destroy: true
 
