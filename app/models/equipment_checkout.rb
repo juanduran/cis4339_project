e121c778420336f32ce99f16683b0c74ec4d5d8c
Ehsan Mahmood <emahmood@uh.edu>
2016-04-17 18:22:26 -0500

    buttons green

--- app/models/equipment_checkout.rb
+++ app/models/equipment_checkout.rb
@@ -2,5 +2,5 @@ class EquipmentCheckout < ActiveRecord::Base
 
   belongs_to :equipment
   belongs_to :employee
-  validates :checkout_date, :return_date,presence: true
+  validates :checkout_date, presence: true
 end
